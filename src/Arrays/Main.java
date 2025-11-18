836246f95cbfb3ba1c8e19a88eba7ccafef48aec
suada <suadaljuaid1@gmail.com>
2025-11-18 11:53:21 +0300

    Lab2 -Arrays--- - part1

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Arrayss arr =new Arrayss();
-         int []numbers=new int[3];
+         int []numbers=new int[5];
         // int []numbers={1,2,3,4,5};
         // System.out.println(Arrays.toString(numbers));
 
