481558d99ec76e9278a992226add0205ab69600c
suada <suadaljuaid1@gmail.com>
2025-11-26 01:42:52 +0300

    Lab3 -Arrays--- - part2

--- src/Arrays/Arrayss.java
+++ src/Arrays/Arrayss.java
@@ -8,7 +8,12 @@ Scanner scanner =new Scanner(System.in);
 
     public void Traversal(int []a){
         for (int i=0;i<a.length;i++)
-            System.out.println(a[i]);
+            System.out.println("Index["+i+"] = "+a[i]);
+    }
+
+    public void Reverse_Traversal(int []a){
+        for (int i=a.length-1;i>=0;i--)
+            System.out.println("Index["+i+"] = "+a[i]);
     }
 
     public void Traversal2(int []a){
@@ -20,6 +25,38 @@ Scanner scanner =new Scanner(System.in);
     System.out.println("Please enter "+a.length+" elements:");
         for(int i=0 ; i<a.length;i++)
          a[i]=scanner.nextInt();
+}
+
+public void update(int []a, int index, int nvlue){
+        if(index <0 || index >=a.length)
+            System.out.println("Index not found");
+        else
+      a[index]=nvlue;
+}
+
+    public void delete(int []a, int index){
+        if(index <0 || index >=a.length)
+            System.out.println("Index not found");
+        else
+            a[index]=0;
+    }
+
+
+public void Shift_Left_Delete(int[]a, int index){
+        for(int i =index; i<a.length-1;i++)
+            a[i]=a[i+1];
+
+       a[a.length-1]=Integer.MIN_VALUE;
+
+}
+
+public int LinearSearch(int[]a, int svalue){
+     for (int i=0 ; i<a.length; i++)
+     {
+         if(svalue==a[i])
+             return i;
+     }
+ return -1;
 }
 
 }
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -6,14 +6,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Arrayss arr =new Arrayss();
-         int []numbers=new int[5];
-        // int []numbers={1,2,3,4,5};
+        // int []numbers=new int[5];
+         int []numbers={1,2,3,4,5};
+         int result= arr.LinearSearch(numbers,8);
+
+
+         if(result!=-1)
+             System.out.println("Value found at index :"+result);
+         else
+             System.out.println("Value not found");
         // System.out.println(Arrays.toString(numbers));
 
-        arr.input(numbers);
+       // arr.input(numbers);
 
-         arr.Traversal(numbers);
-        arr.Traversal2(numbers);
+      //   arr.Traversal(numbers);
+       // arr.Reverse_Traversal(numbers);
+
+       // arr.update(numbers,2,7);
+
+
+        //arr.Traversal(numbers);
 
 
 
